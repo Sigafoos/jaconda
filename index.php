c08b6c4894b4781fe2f16b7cd6a876a1e705b496
Dan Conley <dan.j.conley@gmail.com>
2015-05-20 01:10:36 +0000

    Oh right it might help if I add these

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+require('Slim/Slim/Slim.php');
+require('constants.php');
+\Slim\Slim::registerAutoloader();
+$app = new \Slim\Slim();
+
+function format_url($app)
+{
+	$group_id = $app->request->params('group_id');
+	if (!is_numeric($group_id))
+	{
+		throw new Exception('Bad/empty group_id');
+	}
+	return 'https://jaconda.im/api/v1/endpoint/groups/' . $group_id . '/messages?token=' . TOKEN . '&text=';
+}
+
+function send_curl($url, $text)
+{
+	$ch = curl_init($url . urlencode($text));
+	curl_exec($ch);
+}
+	
+$app->group('/' . KEY, function () use ($app)
+		{
+		$app->post('/wolf', function() use ($app)
+			{
+			$people = array(
+				'Dan',
+				'Elizabeth',
+				'Patricia',
+				'Zach',
+				'Amanda',
+				'Paul',
+				'Matt',
+				'Sam',
+				);
+			$message = 'I am the seer! ' . $people[array_rand($people)] . ' is the wolf!';
+			send_curl(format_url($app), $message);
+			});
+
+		});
+
+$app->run();
+?>
